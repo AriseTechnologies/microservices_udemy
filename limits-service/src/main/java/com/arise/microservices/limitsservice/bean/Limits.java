10d14e1e89003a756ae36d7ea70d0a2a6f2f7bca
Umesh <umesh.manikdurge200@gmail.com>
2024-01-29 12:09:04 +0530

    limit service by Udemy

---
+++ limits-service/src/main/java/com/arise/microservices/limitsservice/bean/Limits.java
@@ -0,0 +1,32 @@
+package com.arise.microservices.limitsservice.bean;
+
+public class Limits {
+
+	int minimum;
+	int maximum;
+	public Limits() {
+		super();
+	}
+	public Limits(int minimum, int maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	@Override
+	public String toString() {
+		return "Limits [minimum=" + minimum + ", maximum=" + maximum + "]";
+	}
+	
+}
