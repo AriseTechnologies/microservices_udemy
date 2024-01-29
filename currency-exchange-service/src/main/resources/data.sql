08ac2e73583927de53ff24ad919efe128740107b
Umesh <umesh.manikdurge200@gmail.com>
2024-01-29 12:35:23 +0530

    Microservice-1 by Udemy

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+Insert into CURRENCY_EXCHANGE
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(101,'USD','INR',80,'');
+Insert into CURRENCY_EXCHANGE
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(102,'EUR','INR',60,'');
+
+Insert into CURRENCY_EXCHANGE
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(103,'AUD','INR',25,'');
